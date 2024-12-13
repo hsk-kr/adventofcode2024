f17eb412933abe34f15115f4918736181b82ce6d
hsk-kr <hsk.coder@gmail.com>
2024-12-13 15:57:59 +0000

    day12 part1, don't know why it's not working...

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -14,30 +14,66 @@ type position struct {
   y int
 }
 
-func readInput() []string {
+func readInput() [][]rune {
   content, err := os.ReadFile("./input")
 
   if err != nil {
     panic(err)
   }
 
-  return strings.Split(strings.TrimSpace(string(content)), "\n")
-}
 
-func yeah(r rune,m []string, visited map[position]bool, pos position, mArea map[position]bool, mPerimeter map[position]bool) {
+  lines := strings.Split(strings.TrimSpace(string(content)), "\n")
+
+  input := make([][]rune, len(lines))
+
+  for i, line := range lines {
+    input[i] = []rune(line)
+  }
+
+  return input
+}
+func yeah(r rune,m [][]rune, visited map[position]bool, pos position, mArea map[position]bool, mPerimeter map[position]bool) {
   x := pos.x
   y := pos.y
 
-  if x < 0 || y < 0 || x >= len(m[1]) || y >= len(m) || visited[pos] || rune(m[y][x]) != r {
+  if x < 0 || y < 0 || x >= len(m[1]) || y >= len(m) || visited[pos] || m[y][x] != r {
     return
   }
 
   visited[pos] = true
   mArea[position{ x, y }] = true
-  mPerimeter[position{ x, y }] = !(mArea[position{ x: x - 1, y: y }] && mArea[position{ x: x - 1, y: y - 1 }] && mArea[position{ x: x, y: y - 1}])
-  mPerimeter[position{ x: x, y: y + 1 }] = !(mArea[position{ x: x - 1, y: y }] && mArea[position{ x: x - 1, y: y + 1 }] && mArea[position{ x: x, y: y + 1}])
-  mPerimeter[position{ x: x + 1, y: y }] = !(mArea[position{ x: x + 1, y: y }] && mArea[position{ x: x + 1, y: y - 1 }] && mArea[position{ x: x, y: y - 1}])
-  mPerimeter[position{ x: x + 1, y: y + 1 }] = !(mArea[position{ x: x + 1, y: y }] && mArea[position{ x: x + 1, y: y + 1 }] && mArea[position{ x: x, y: y + 1}])
+
+  mPerimeter[position{ x, y }] = 
+    !mArea[position{ x: x - 1, y: y }] ||
+    !mArea[position{ x: x - 1, y: y - 1 }] || 
+    !mArea[position{ x: x, y: y - 1}]
+  if !mPerimeter[position{ x, y }] {
+    delete(mPerimeter, position{ x, y })
+  }
+  
+  mPerimeter[position{ x: x + 1, y: y }] = 
+    !mArea[position{ x: x + 1, y: y }] ||
+    !mArea[position{ x: x + 1, y: y - 1 }] ||
+    !mArea[position{ x: x, y: y - 1}]
+  if !mPerimeter[position{ x: x + 1, y: y }] {
+    delete(mPerimeter, position{ x: x + 1, y: y })
+  }
+
+  mPerimeter[position{ x: x, y: y + 1 }] = 
+    !mArea[position{ x: x - 1, y: y }] ||
+    !mArea[position{ x: x - 1, y: y + 1 }] ||
+    !mArea[position{ x: x, y: y + 1}]
+  if !mPerimeter[position{ x: x, y: y + 1 }] {
+    delete(mPerimeter, position{ x: x, y: y + 1 })
+  }
+
+  mPerimeter[position{ x: x + 1, y: y + 1 }] = 
+    !mArea[position{ x: x + 1, y: y }] ||
+    !mArea[position{ x: x + 1, y: y + 1 }] ||
+    !mArea[position{ x: x, y: y + 1}]
+  if !mPerimeter[position{ x: x + 1, y: y + 1 }] {
+    delete(mPerimeter, position{ x: x + 1, y: y + 1 })
+  }
 
   yeah(r, m, visited, position{ x: x - 1, y: y}, mArea, mPerimeter)
   yeah(r, m, visited, position{ x: x + 1, y: y}, mArea, mPerimeter)
@@ -50,6 +86,7 @@ func main() {
   visited := make(map[position]bool)
 
   answer := 0
+  totalRunes := 0
   for y, row := range m {
     for x, r := range row {
       mPerimeter := make(map[position]bool)
@@ -60,18 +97,9 @@ func main() {
       }
       yeah(r, m, visited, position{ x, y }, mArea, mPerimeter)
 
-      // if len(mArea) > 0 {
-      //   fmt.Println(r, mArea, "/", mPerimeter)
-      // }
-
       numArea := len(mArea)
-      numPerimeter := 0
-      
-      for _, v := range mPerimeter {
-        if v {
-          numPerimeter++
-        }
-      }
+      numPerimeter := len(mPerimeter)
+      totalRunes += numArea
 
       if (numArea != 0) {
         answer += numArea * numPerimeter
@@ -80,5 +108,5 @@ func main() {
     }
   }
 
-  fmt.Println(answer)
+  fmt.Println("answer", answer, "totalRunes", totalRunes, 140*140)
 }
